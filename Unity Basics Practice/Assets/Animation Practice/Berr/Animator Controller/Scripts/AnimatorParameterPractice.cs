face9413be33b65e6f0e27b54d354fea5389ba28
Kim Seongho <yrs03143@gmail.com>
2022-05-13 15:03:50 +0900

    베르 애니메이터 컨트롤러
    https://www.youtube.com/watch?v=hOrH3aUSn64

---
+++ Unity Basics Practice/Assets/Animation Practice/Berr/Animator Controller/Scripts/AnimatorParameterPractice.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorParameterPractice : MonoBehaviour
+{
+    // 한글
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
